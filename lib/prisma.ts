f3e5a5e88e43fae3013e2417319d535efe9f8567
phabianmalonja3 <phabianmalonja3@gmail.com>
2025-12-30 17:14:30 +0300

    new commit

--- lib/prisma.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-import { PrismaPg } from '@prisma/adapter-pg'
-
-import { PrismaClient } from '../prisma/generated/client'
-const connectionString = `${process.env.DATABASE_URL}`
-
-const adapter = new PrismaPg({ connectionString })
-export const  prisma = new PrismaClient({ adapter })
