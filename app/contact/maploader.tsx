80649ea41699a1ed09250fbf4505ef17f0f9413f
phabianmalonja3 <phabianmalonja3@gmail.com>
2025-12-30 17:39:19 +0300

    new commit

--- app/contact/maploader.tsx
+++
@@ -1,8 +0,0 @@
-import React, { Suspense } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-
-export const MapLoader = () => (
-  <div className="w-full h-full flex items-center justify-center bg-gray-100">
-    <p className="text-gray-500">Loading map...</p>
-  </div>
-);
