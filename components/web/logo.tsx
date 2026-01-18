93918e9e394feeee05623b262f5d135044eee7fd
phabianmalonja3 <phabianmalonja3@gmail.com>
2026-01-18 23:30:19 +0300

    new commit

--- components/web/logo.tsx
+++ components/web/logo.tsx
@@ -1,19 +1,24 @@
 import Image from 'next/image';
 import React from 'react';
-
 import logo from '../../public/logo.png';
+import { cn } from "@/lib/utils"; // Import cn to merge classes safely
 
-const Logo = () => {
-    return <Image
-                  className="dark:invert"
-                  src={logo}
-                  alt="Next.js logo"
-                  width={100}
-                  height={20}
-                  sizes=''
-                  priority
-                />
-    
+interface LogoProps {
+  className?: string;
 }
+
+const Logo = ({ className }: LogoProps) => {
+  return (
+    <Image
+      // Using cn allows you to keep 'dark:invert' and add new classes from Nav
+      className={cn("dark:invert", className)}
+      src={logo}
+      alt="Mikumi Logo"
+      width={100}
+      height={20}
+      priority
+    />
+  );
+};
 
 export default Logo;
