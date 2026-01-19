ca9cd7ca1e8ca6a13cabca8414f0081e16c8fac3
phabianmalonja3 <phabianmalonja3@gmail.com>
2026-01-19 18:47:11 +0300

    config the domain

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+
+  // Logging (optional)
   logging: {
     fetches: {
       fullUrl: true,
     },
   },
-  experimental: {
-    allowedDevOrigins: ["http://localhost:3000", "http://127.0.0.1:3000"],
-  },
+
   images: {
+    // Allow images served through your domain via NGINX
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '188.245.179.190',
-        port: '8000',
-        pathname: '/storage/**',
-      },
-      // ADD THIS SECTION FOR LOCALHOST
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
+        protocol: 'https',
+        hostname: 'mikumisafari.co.tz',
+        port: '', // leave blank for default HTTPS port 443
         pathname: '/storage/**',
       },
     ],
